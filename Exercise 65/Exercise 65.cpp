5bcce1b65b9c2034646d02e091767d7b100beb2d
Eng <jveng@engineering.cpp.edu>
2020-03-11 11:36:57 -0700

    Add project files.

---
+++ Exercise 65/Exercise 65.cpp
@@ -0,0 +1,29 @@
+// Exercise 65.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int square(int x);
+double square(double x);
+int main()
+{
+    cout << square(3)<<"\n";
+    cout << square(.9);
+}
+int square(int x) {
+    return x * x;
+}
+double square(double x) {
+    return x * x;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
